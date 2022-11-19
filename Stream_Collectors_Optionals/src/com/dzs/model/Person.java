0203d6bca86456572188b06303d7e51d37195bda
usb777 <dzsamoila@gmail.com>
2022-11-19 13:04:52 -0500

    Dev_Master collections

---
+++ Stream_Collectors_Optionals/src/com/dzs/model/Person.java
@@ -0,0 +1,5 @@
+package com.dzs.model;
+
+public class Person {
+
+}
---
+++ Stream_Collectors_Optionals/src/com/dzs/spliterator/SpliteratorChild.java
@@ -0,0 +1,10 @@
+package com.dzs.spliterator;
+
+public class SpliteratorChild {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
